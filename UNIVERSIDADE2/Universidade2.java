c468ca337af5f7678e7b7ae982d516391bd14a72
Felipe Ribeiro <felipe.cunha46@etec.sp.gov.br>
2024-02-11 16:30:47 -0300

    initial commit

---
+++ UNIVERSIDADE2/Universidade2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Universidade2
+{
+public static void main(String[] args)
+{
+int tempo;
+String universidade;
+Scanner scan = new Scanner(System.in);
+System.out.print(“Onde voce estuda: ”);
+universidade = scan.next();
+System.out.print(“Quanto tempo: ”);
+tempo = scan.nextInt();
+System.out.println(“Voce estuda na ” + universidade + “ ha ”
++ tempo + “ anos.”);
+
+}
+}
